927637c6002eb27efe3abd12aa4cfa12ec14b6a8
Mavdavis <ekwesimarvelous@gmail.com>
2023-01-28 18:33:00 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Setup from "./tutorial/6-useReducer/setup";
+import Setup from "./tutorial/11-react-router/setup";
 function App() {
   return (
     <div className="container">
-      <Setup /> 
-      
+      <Setup />
     </div>
   );
 }
